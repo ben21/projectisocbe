71b93feda624c0fcd2d98088b8c03e01b445876e
ben21 <bvanbever1@gmail.com>
2019-06-18 15:48:28 +0200

    final draft before AG

--- members/models.py
+++ members/models.py
@@ -2,19 +2,19 @@ from django.db import models
 from django.urls import reverse
 
 class Member(models.Model):
-    prefix = models.CharField(max_length=255)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.CharField(max_length=255)
+    prefix = models.CharField(max_length=11)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=30)
+    email_address = models.EmailField(max_length=255)
     country = models.CharField(max_length=255)
     street_address = models.CharField(max_length=255)
     locality = models.CharField(max_length=255)
     post_address = models.CharField(max_length=255)
-    professional_phone = models.CharField(max_length=255)
-    private_phone = models.CharField(max_length=255)
-    fax_number = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    organization = models.CharField(max_length=255)
+    professional_phone = models.CharField(max_length=20)
+    private_phone = models.CharField(max_length=25)
+    fax_number = models.CharField(max_length=25)
+    title = models.CharField(max_length=25)
+    organization = models.CharField(max_length=40)
 
     def __str__(self):
         return self.last_name
